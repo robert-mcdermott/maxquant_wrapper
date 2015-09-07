333c8c4710097e82b197c0501b10d6c546c3dfc8
Christian Panse <cp@fgcz.ethz.ch>
2015-09-07 14:56:37 +0200

    basic rpc functionallity

--- fgcz_maxquant_rpc_server.py
+++ fgcz_maxquant_rpc_server.py
@@ -7,26 +7,71 @@ import os
 import xmlrpclib
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
+from optparse import OptionParser
+
+
+import time
+
+import subprocess
 
 class FgczMaxquantWrapper:
+    """
+
+    """
+    config = None
+
     def __init__(self):
         pass
 
-    def load_config(self, config):
-        print config
-        #self.config = config
+    def run_commandline(self, cmd):
+
+        (pid, return_code) = (None, None)
+
+        tStart = time.time()
+        try:
+            p = subprocess.Popen(cmd, shell=True)
+
+            pid = p.pid
+            return_code = p.wait()
+
+            (out, err) = p.communicate()
+            p.terminate()
+
+        except OSError as e:
+            msg = "exception|pid={0}|OSError=".format(pid, e)
+            print msg
+
+        msg_info = "completed|pid={0}|time={1}|return_code={2}|cmd='{3}'".format(pid, time.time() - tStart, return_code, cmd)
+        print msg_info
+
+        print out
+        return (return_code)
 
     def print_config(self):
         print self.config
 
-    def add(self, a, b):
-        print b
-        return 1
+    def add_configuration(self, config):
+        self.config = config
+        return True
 
 
 if __name__ == "__main__":
+    parser = OptionParser(usage="usage: %prog -h <hostname>",
+                          version="%prog 1.0")
+
+    parser.add_option("-n", "--hostname",
+                      type='string',
+                      action="store",
+                      dest="hostname",
+                      default="localhost",
+                      help="provide a hostname")
+
+    (options, args) = parser.parse_args()
+
     mqw = FgczMaxquantWrapper()
 
-    server = SimpleXMLRPCServer(("130.60.81.113", 8082))
+    server = SimpleXMLRPCServer((options.hostname, 8082))
+
     server.register_instance(mqw)
+
     server.serve_forever()

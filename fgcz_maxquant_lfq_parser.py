49faee0ca8ac977130ce9740f32a05b6388fa81c
Christian Panse <cp@fgcz.ethz.ch>
2015-10-06 14:26:33 +0200

    working

--- fgcz_maxquant_lfq_parser.py
+++ fgcz_maxquant_lfq_parser.py
@@ -1,10 +1,53 @@
 #!/usr/local/bin/python3
-# Simon Barkow-Oesterreicher 
+# Simon Barkow-Oesterreicher
 # 20140218
 
-def parseHeader(lines):
+
+def parseHeaderLFQintensity(lines):
+    #print("parseHeaderLFQintensity called")
+    # get indexes for Intensity columns
+    i = 0
+    j = 0
+    localIntensityIndexLFQ = 0
+    for h in lines[0]:
+        if h.startswith('LFQ intensity'):
+            #print("intensity found")
+            #print(h)
+            if j == 0:
+                localIntensityIndexLFQ = i
+            #print("debug first index: ")
+            #print(localIntensityIndexLFQ)
+            localLastIntensityForFileIndexLFQ = i
+            j += 1
+        i += 1
+    
+    return localIntensityIndexLFQ, localLastIntensityForFileIndexLFQ
+
+def parseHeaderMSMScounts(lines):
+    #print("parseHeaderLFQintensity called")
+    # get indexes for Intensity columns
+    i = 0
+    j = 0
+    localMSMSIndexLFQ = 0
+    for h in lines[0]:
+        if h.startswith('MS/MS Count'):
+            #print("intensity found")
+            #print(h)
+            if j == 0:
+                localMSMSIndexLFQ = i
+            #print("debug first index: ")
+            #print(localIntensityIndexLFQ)
+            localLastMSMSForFileIndexLFQ = i
+            j += 1
+        i += 1
+    
+    return localMSMSIndexLFQ, localLastMSMSForFileIndexLFQ
+
+
+def parseHeaderIntensity(lines):
     # get indexes for Intensity coulumns
     i = 0
+    intensityIndex = 0
     for h in lines[0]:
         if h == 'Intensity':
             intensityIndex = i
@@ -14,33 +57,57 @@ def parseHeader(lines):
     
     return intensityIndex, lastIntensityForFileIndex
 
-def filterForQuantValues(inputFileLines, outputFilePath):
-    intensityIndex, lastIntensityForFileIndex = parseHeader(inputFileLines)
-    numberOfFiles = lastIntensityForFileIndex - intensityIndex 
+def filterForLFQintensityQuantValues(inputFileLines, outputFilePath):
+    intensityIndex, lastIntensityForFileIndex = parseHeaderLFQintensity(inputFileLines)
+    numberOfFiles = lastIntensityForFileIndex - intensityIndex
     outFile = open(outputFilePath, "w")
-    for l in inputFileLines: 
+    for l in inputFileLines:
+        print(l[1], file = outFile, end='\t')
+        for item in l[(lastIntensityForFileIndex)-numberOfFiles:lastIntensityForFileIndex]:
+            print(item, file = outFile,  end='\t')
+        for item in l[lastIntensityForFileIndex:lastIntensityForFileIndex+1]:
+            print(item, file = outFile,  end='\n')
+
+def filterForMSMScountValues(inputFileLines, outputFilePath):
+    MSMScountIndex, lastMSMScountForFileIndex = parseHeaderMSMScounts(inputFileLines)
+    numberOfFiles = lastMSMScountForFileIndex - MSMScountIndex
+    outFile = open(outputFilePath, "w")
+    for l in inputFileLines:
+        print(l[1], file = outFile, end='\t')
+        for item in l[(lastMSMScountForFileIndex)-numberOfFiles:lastMSMScountForFileIndex]:
+            print(item, file = outFile,  end='\t')
+        for item in l[lastMSMScountForFileIndex:lastMSMScountForFileIndex+1]:
+            print(item, file = outFile,  end='\n')
+
+
+
+def filterForIntensityQuantValues(inputFileLines, outputFilePath):
+    intensityIndex, lastIntensityForFileIndex = parseHeaderIntensity(inputFileLines)
+    numberOfFiles = lastIntensityForFileIndex - intensityIndex
+    outFile = open(outputFilePath, "w")
+    for l in inputFileLines:
         print(l[1], file = outFile, end='\t')
         for item in l[(lastIntensityForFileIndex+1)-numberOfFiles:lastIntensityForFileIndex]:
             print(item, file = outFile,  end='\t')
         for item in l[lastIntensityForFileIndex:lastIntensityForFileIndex+1]:
             print(item, file = outFile,  end='\n')
-    
-    
+
+
 def checkFile(inputFileLines):
     numberOfColumns=len(inputFileLines[0])
     for line in inputFileLines:
-        if len(line) != numberOfColumns: 
+        if len(line) != numberOfColumns:
             return False
-    return True    
+    return True
 
 def removeColumnByHeaderName(inputFileLines, outputFilePath, columnName):
     header = inputFileLines[0]
     indexOfFastaDesctiptionColumn= header.index(columnName)
     outFile = open(outputFilePath, "w")
     print('\t'.join(header),file = outFile, end='')
-    for l in inputFileLines[1:]:     
+    for l in inputFileLines[1:]:
         print('\t'.join(l[:indexOfFastaDesctiptionColumn]+[""]+l[indexOfFastaDesctiptionColumn+1:]), file = outFile, end='')
-    
+
 import sys, getopt
 
 def main(argv):
@@ -48,12 +115,12 @@ def main(argv):
     outputFilePath = ''
     function = ''
     if len(argv) != 6:
-        print('MaxQuant_LFQ_parser.py -i <inputFilePath> -o <outputFilePath> -u <removeColumn|filter>')
+        print('MaxQuant_LFQ_parser.py -i <inputFilePath> -o <outputFilePath> -u <removeColumn|filterIntensity|filterLFQintensity|filterMSMScounts>')
         sys.exit()
     try:
         opts,arg = getopt.getopt(argv,"hi:o:u:",["ifile=","ofile=","function="])
     except getopt.GetoptError:
-        print('MaxQuant_LFQ_parser.py -i <inputFilePath> -o <outputFilePath> -u <removeColumn|filter>')
+        print('MaxQuant_LFQ_parser.py -i <inputFilePath> -o <outputFilePath> -u <removeColumn|filterIntensity|filterLFQintensity|filterMSMScounts>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -77,15 +144,20 @@ def main(argv):
         print("The input file seems to be corrupt")
         sys.exit()
     
-    if function == 'filter':
-        filterForQuantValues(inputFileLines, outputFilePath)
+    if function == 'filterIntensity':
+        filterForIntensityQuantValues(inputFileLines, outputFilePath)
+    elif function == 'filterLFQintensity':
+        filterForLFQintensityQuantValues(inputFileLines, outputFilePath)
+    elif function == 'filterMSMScounts':
+        filterForMSMScountValues(inputFileLines, outputFilePath)
     elif function == 'removeColumn':
         removeColumnByHeaderName(inputFileLines, outputFilePath, 'Fasta headers')
     else:
         print("no function given. Exit.")
-        sys.exit()   
-        
-            
+        sys.exit()
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
+
 
--- fgcz_sge_qc.bash
+++ fgcz_sge_qc.bash
@@ -30,22 +30,27 @@ cp -v proteinGroups.txt proteinGroups.txt.bak
 cp -v evidence.txt evidence.txt.bak
 
 # Select which MQ_resultOverview to take based on RT-Kit and iRT_Protein
-if grep --quiet RT-Kit proteinGroups.txt; then 
-	echo "RT-Kit found in proteinGroups.txt"
-	cp $SWEAVEDIR/LFQ_MQ_Overview_n_QC_RT-Kit.Rnw QCSweaveFile.Rnw
-elif grep --quiet iRT_Protein proteinGroups.txt; then
-	echo "iRT_Protein found in proteinGroups.txt"
-	cp $SWEAVEDIR/LFQ_MQ_Overview_n_QC_iRT_Protein.Rnw QCSweaveFile.Rnw
-else  
-	echo "neither iRT_Protein nor RT-Kit found in proteinGroups.txt"
-	cp $SWEAVEDIR/LFQ_MQ_Overview_n_QC_no_iRT_Protein.Rnw QCSweaveFile.Rnw
-fi
+#if grep --quiet RT-Kit proteinGroups.txt; then 
+#	echo "RT-Kit found in proteinGroups.txt"
+#	cp $SWEAVEDIR/LFQ_MQ_Overview_n_QC_RT-Kit.Rnw QCSweaveFile.Rnw
+#elif grep --quiet iRT_Protein proteinGroups.txt; then
+#	echo "iRT_Protein found in proteinGroups.txt"
+#	cp $SWEAVEDIR/LFQ_MQ_Overview_n_QC_iRT_Protein.Rnw QCSweaveFile.Rnw
+#else  
+#	echo "neither iRT_Protein nor RT-Kit found in proteinGroups.txt"
+#	cp $SWEAVEDIR/LFQ_MQ_Overview_n_QC_no_iRT_Protein.Rnw QCSweaveFile.Rnw
+#fi
 
 # copying the graphics reqired by the by the Rnw files
+cp $SWEAVEDIR/MQ_resultOverview.Rnw QCSweaveFile.Rnw
 cp -v $SWEAVEDIR/graphics/*.pdf .
+cp -v $SWEAVEDIR/QprotMatrixFunctions_rn_V2.R .
 
 # clean up proteinGroups.txt and evidence.txt for Sweave by removing the Fasta description
-/usr/bin/python3 $WRAPPERDIR/fgcz_maxquant_lfq_parser.py -i evidence.txt -o evidence.txt -u removeColumn
+/usr/bin/python3 $WRAPPERDIR/fgcz_maxquant_lfq_parser.py -i evidence.txt -o evidence.txt -u removeColumn 
+/usr/bin/python3 $WRAPPERDIR/fgcz_maxquant_lfq_parser.py -i proteinGroups.txt -o proteinGroups.txt -u removeColumn
+/usr/bin/python3 $WRAPPERDIR/fgcz_maxquant_lfq_parser.py -i proteinGroups.txt -o proteinGroups_FGCZ2grp_Intensity.txt -u filterIntensity
+
 test $? -gt 0 && { echo "cleaning up evidence.txt failed"; exit 1; }
 
 /usr/bin/python3 $WRAPPERDIR/fgcz_maxquant_lfq_parser.py -i proteinGroups.txt -o proteinGroups.txt -u removeColumn
